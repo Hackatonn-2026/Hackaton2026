491573592e5b9eecc99cc7e94083f9f8403cda57
NicolasMartinsPaiva <paivanicolasmartins@gmail.com>
2026-07-24 15:27:31 -0300

    tirando o import do router link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import AppHeader from './layout/AppHeader.vue';
 import TheFooter from './layout/TheFooter.vue';
-import { RouterLink, RouterView } from 'vue'
 </script>
 
 <template>
