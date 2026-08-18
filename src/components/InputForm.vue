8c27b5de0a4c42d9727edcb97333fe4eb6db4e8d
carloslvr <carloslvr09@gmail.com>
2026-08-18 10:47:41 -0300

    Começo código input

---
+++ src/components/InputForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="input-group">
+    <label>{{ label }}</label>
+    <inputForm
+      :type="type"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: String,
+  placeholder: String,
+  type: { type: String, default: 'text' },
+  modelValue: String,
+});
+defineEmits(['update:modelValue']);
+</script>
+
+<style scoped>
+
+</style>
