c9dff514b19d3770f33c4f24e2a153ee5a1e2559
carloslvr <carloslvr09@gmail.com>
2026-08-18 11:15:29 -0300

    input pronto ou quase pronto

---
+++ src/components/CadastroForm.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { ref } from 'vue'
+const tipoUsuario = ref('freelancer')
+</script>
+
+<template>
+  <select v-model="tipoUsuario">
+    <option value="freelancer">Freelancer</option>
+    <option value="contratante">Contratante</option>
+  </select>
+
+  <InputForm
+    label="Nome"
+    placeholder="Digite seu nome"
+    v-model="nome"
+  />
+
+  <InputForm
+    label="E-mail"
+    placeholder="Digite seu e-mail"
+    type="email"
+    v-model="email"
+  />
+
+  <!-- Documentos do contratante -->
+  <div v-if="tipoUsuario === 'contratante'">
+    
+    <InputForm
+      label="CPF ou CNPJ"
+      placeholder="Digite seu CPF ou CNPJ"
+      v-model="identificacao"
+    />
+
+    <InputForm
+      label="Telefone"
+      placeholder="Digite seu telefone"
+      v-model="telefone"
+    />
+
+    </div>
+
+  <!-- Documentos do freelancer -->
+  <div v-if="tipoUsuario === 'freelancer'">
+
+    <InputForm
+      label="CPF ou CNPJ"
+      placeholder="Digite seu CPF ou CNPJ"
+      v-model="identificacao"
+    />
+
+    <InputForm
+    label="Telefone"
+    placeholder="Digite seu telefone"
+    v-model="telefone"
+    />
+
+    <InputForm
+      label="Portfólio"
+      placeholder="Link do portfólio"
+      v-model="portfolio"
+    />
+
+  </div>
+  
+</template>
